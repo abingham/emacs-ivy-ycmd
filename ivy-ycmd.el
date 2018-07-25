373c65ccd349fe1b5372b9c4c998426854badc43
Austin Bingham <austin.bingham@gmail.com>
2018-07-25 18:24:12 -0500

    Removed some unnecessarys stuff.

--- ivy-ycmd.el
+++ ivy-ycmd.el
@@ -20,24 +20,18 @@
 
 ;;; Commentary:
 
+;; Description:
 ;;
+;; This adds an ivy-based interface to some of the features of ycmd. For
+;; instance, it lets you access references to a name through ivy.
 
 ;;; Code:
 
 (require 'ivy)
 (require 'ycmd)
 
-(defgroup ivy-ycmd nil
-  "Variables related to ivy-ycmd"
-  :prefix "ivy-ycmd-"
-  :group 'tools)
-
-(defcustom ivy-ycmd-mininum-input-length 1
-  "The minimum number of input characters before running a search."
-  :type 'integer)
-
 (defun ivy-ycmd--handle-selection (selection)
-  "Jump to the file/line indicated by SELECTION."
+  "Jump to the file/line indicated by `selection'."
   (with-ivy-window
     (save-match-data
       (let* ((loc-data (cdr selection))
@@ -48,32 +42,38 @@
         (goto-char (point-min))
         (forward-line (- line-number 1))))))
 
-(defun ivy-ycmd--handle-response (response initial-input)
+(defun ivy-ycmd--make-selection-entry (location)
+  "Create an ivy selection entry from `location'.
+
+The entry will be a cons-sell with a display-string as the car
+and `location' as the cdr."
+  (cons (make-symbol
+         (format "%s:%s\t%s"
+                 (cdr (assq 'filepath location))
+                 (cdr (assq 'line_num location))
+                 (cdr (assq 'description location))))
+        location))
+
+(defun ivy-ycmd--handle-response (response)
+  "List the file locations in `response' with ivy."
   (ivy-read "Goto: "
-            (mapcar
-             (lambda (f)
-               (cons (make-symbol  (format "%s:%s\t%s"
-                                           (cdr (assq 'filepath f))
-                                           (cdr (assq 'line_num f))
-                                           (cdr (assq 'description f))))
-                     f))
-             response)
-            :initial-input initial-input
-            :history #'ivy-ycmd-history
+            (mapcar #'ivy-ycmd--make-selection-entry response)
             :action #'ivy-ycmd--handle-selection
-            :caller 'ivy-ycmd)
-  )
+            :caller 'ivy-ycmd))
 
 ;;;###autoload
-(defun ivy-ycmd (&optional initial-input)
-  "TODO"
+(defun ivy-ycmd-goto-references ()
+  "Jump to a reference to the symbol at the current point.
+
+This finds all references to the symbol at point, lists them with
+ivy, and jumps to the one selected by the user."
   (interactive)
   (save-excursion
     (--when-let (bounds-of-thing-at-point 'symbol)
       (goto-char (car it)))
     (ycmd--run-completer-command "GoToReferences"
       (lambda (response)
-        (ivy-ycmd--handle-response response initial-input)))))
+        (ivy-ycmd--handle-response response)))))
 
 (provide 'ivy-ycmd)
 ;;; ivy-ycmd.el ends here
